4649b61ed6e0c97e53aaf90a8552ddc26daad776
Vincent Tiu <vincetiu8@gmail.com>
2024-02-16 14:50:39 -0500

    initialize plasmo repo

---
+++ extension/popup.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function IndexPopup() {
+  return (
+    <div
+      style={{
+        padding: 16
+      }}>
+      <h2>
+        I'm watching you :)
+      </h2>
+    </div>
+  )
+}
+
+export default IndexPopup
