3d7cf59accf951fdbf82edfd7d0562228584fc53
benjmnxu <benxu@seas.upenn.edu>
2024-02-16 17:54:51 -0500

    os

--- backend/llm.py
+++ backend/llm.py
@@ -1,25 +1,26 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Replace the empty string with your model id below
-model_id = "6wglroj3"
+model_id = os.environ["BASE_TEN_MODEL_ID"]
 
-print(os.environ["BASE_TEN_API_KEY"])
+client = OpenAI(
+   api_key=os.environ["BASE_TEN_API_KEY"],
+   base_url=f"https://bridge.baseten.co/{model_id}/v1"
+)
 
-# client = OpenAI(
-#    api_key="5B4ADGY0.kheOS4oGtjvewwF5FQIHubKhx2MzlXMt",
-#    base_url=f"https://bridge.baseten.co/{model_id}/v1"
-# )
+# Call model endpoint
+res = client.chat.completions.create(
+ model="mistral-7b",
+ messages=[
+   {"role": "user", "content": "What is a mistral?"},
+   {"role": "assistant", "content": "A mistral is a type of cold, dry wind that blows across the southern slopes of the Alps from the Valais region of Switzerland into the Ligurian Sea near Genoa. It is known for its strong and steady gusts, sometimes reaching up to 60 miles per hour."},
+   {"role": "user", "content": "How does the mistral wind form?"}
+ ],
+ temperature=0.9,
+ max_tokens=512,
+)
 
-# res = client.chat.completions.create(
-#  model="mistral-7b",
-#  messages=[
-#    {"role": "user", "content": "What is a mistral?"},
-#    {"role": "assistant", "content": "A mistral is a type of cold, dry wind that blows across the southern slopes of the Alps from the Valais region of Switzerland into the Ligurian Sea near Genoa. It is known for its strong and steady gusts, sometimes reaching up to 60 miles per hour."},
-#    {"role": "user", "content": "How does the mistral wind form?"}
-#  ],
-#  temperature=0.9,
-#  max_tokens=512,
-# )
-
-# print(res.choices[0].message.content)
+print(res.choices[0].message.content)
