06709fa4354c511db6e68227e092f00f7781bdb9
benjmnxu <benxu@seas.upenn.edu>
2024-02-17 10:18:46 -0500

    prompts and finding similar people

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -1,6 +1,10 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
 from transformers import AutoTokenizer
+from prompts import find_others_system_prompt, find_others_user_prompt
+
+load_dotenv()
 
 def init_llm():
     model_id = os.environ["BASE_TEN_MODEL_ID"]
@@ -12,13 +16,13 @@ def init_llm():
 def cut(webSequence):
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
     tokenized = tokenizer(webSequence, return_tensors="pt")
-    if (len(tokenized.input_ids[0]) < 4096):
+    if (len(tokenized.input_ids[0]) < 4000):
         return webSequence
-    return tokenizer.decode(tokenized.input_ids[0][:4096])
+    return tokenizer.decode(tokenized.input_ids[0][:4000])
 
 def summarize_webpage(llm: OpenAI, title, url, body):
     webSequence = f"TITLE:{title},URL:{url},BODY:{body}"
-    webSequence = cut(webSequence[:4096])
+    webSequence = cut(webSequence)
     res = llm.chat.completions.create(
     model="mistral-7b",
     messages=[
@@ -34,7 +38,7 @@ def summarize_webpage(llm: OpenAI, title, url, body):
     return arr[1]
     
 def summarize_day(llm: OpenAI, summaries: list[str]):
-    allSummaries = "`".join(summaries)[:4096]
+    allSummaries = "`".join(summaries)[:4000]
     res = llm.chat.completions.create(
     model="mistral-7b",
     messages=[
@@ -48,4 +52,19 @@ def summarize_day(llm: OpenAI, summaries: list[str]):
     if (len(arr) < 1):
         raise ValueError("No output")
     return arr[1]
-
+    
+def other_k_people(llm: OpenAI, target, neighbors):
+    res = llm.chat.completions.create(
+    model="mistral-7b",
+    messages=[
+        {"role": "system", "content": find_others_system_prompt()},
+        {"role": "user", "content": find_others_user_prompt(target, cut(neighbors))}
+        ],
+        temperature=0.6,
+        max_tokens=512,
+    )
+    arr = res.choices[0].message.content.split("[/INST]")
+    if (len(arr) < 1):
+        raise ValueError("No output")
+    return arr[1]
+    
