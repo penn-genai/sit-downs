bd167f82da48350da328065a6f398055c81ba282
Justin Sun <justinsunyt@gmail.com>
2024-02-17 14:40:06 -0500

    idk

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -6,94 +6,111 @@ from utils.prompts import find_others_system_prompt, find_others_user_prompt
 
 load_dotenv()
 
+
 def init_llm():
     model_id = os.environ["BASE_TEN_MODEL_ID"]
     return OpenAI(
         api_key=os.environ["BASE_TEN_API_KEY"],
-        base_url=f"https://bridge.baseten.co/{model_id}/v1"
+        base_url=f"https://bridge.baseten.co/{model_id}/v1",
     )
+
 
 def cut(webSequence):
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
     tokenized = tokenizer(webSequence, return_tensors="pt")
-    if (len(tokenized.input_ids[0]) < 4000):
+    if len(tokenized.input_ids[0]) < 4000:
         return webSequence
     return tokenizer.decode(tokenized.input_ids[0][:4000])
+
 
 def summarize_webpage(llm: OpenAI, title, url, body):
     webSequence = f"TITLE:{title},URL:{url},BODY:{body}"
     webSequence = cut(webSequence)
     res = llm.chat.completions.create(
-    model="mistral-7b",
-    messages=[
-        {"role": "system", "content": "Summarize what the following web page created by this title, URL and DOM does in a short, concise paragraph of LESS THAN 100 WORDS:"},
-        {"role": "user", "content": webSequence}
+        model="mistral-7b",
+        messages=[
+            {
+                "role": "system",
+                "content": "Summarize what the following web page created by this title, URL and DOM does in a short, concise paragraph of LESS THAN 100 WORDS:",
+            },
+            {"role": "user", "content": webSequence},
         ],
         temperature=0.6,
         max_tokens=512,
     )
     arr = res.choices[0].message.content.split("[/INST]")
-    if (len(arr) < 1):
+    if len(arr) < 1:
         raise ValueError("No output")
     return arr[1]
-    
+
+
 def summarize_date(llm: OpenAI, name: str, summaries: list[str]):
-    allSummaries = "`".join(summaries)[:4000]
+    allSummaries = cut("`".join(summaries))
     res = llm.chat.completions.create(
-    model="mistral-7b",
-    messages=[
-        {"role": "system", "content": f"From the following summaries of web pages a user '{name}' has been browsing, infer what they have been working on. Do not give an introduction. Do not use pronouns or an explicit subject. Be in present-continuous tense. Start Each summary is separated by a `"},
-        {"role": "user", "content": allSummaries}
+        model="mistral-7b",
+        messages=[
+            {
+                "role": "system",
+                "content": f"From the following summaries of web pages a user '{name}' has been browsing, infer what they have been working on. Do not give an introduction. Do not use pronouns or an explicit subject. Be in present-continuous tense. Start Each summary is separated by a `",
+            },
+            {"role": "user", "content": allSummaries},
         ],
         temperature=0.6,
         max_tokens=512,
     )
     arr = res.choices[0].message.content.split("[/INST]")
-    if (len(arr) < 1):
+    if len(arr) < 1:
         raise ValueError("No output")
     return arr[1]
+
 
 def summarize_summary(llm: OpenAI, summary):
     res = llm.chat.completions.create(
-    model="mistral-7b",
-    messages=[
-        {"role": "system", "content": "Summarize the following paragraph into one concise sentence with less than 10 words. Be in present-continuous tense."},
-        {"role": "user", "content": summary}
+        model="mistral-7b",
+        messages=[
+            {
+                "role": "system",
+                "content": "Summarize the following paragraph into one concise sentence with less than 10 words. Be in present-continuous tense.",
+            },
+            {"role": "user", "content": summary},
         ],
         temperature=0.6,
         max_tokens=512,
     )
     arr = res.choices[0].message.content.split("[/INST]")
-    if (len(arr) < 1):
+    if len(arr) < 1:
         raise ValueError("No output")
     return arr[1]
 
-    
+
 def other_k_people(llm: OpenAI, target, neighbors):
     res = llm.chat.completions.create(
-    model="mistral-7b",
-    messages=[
-        {"role": "system", "content": find_others_system_prompt()},
-        {"role": "user", "content": find_others_user_prompt(target, cut(neighbors))}
+        model="mistral-7b",
+        messages=[
+            {"role": "system", "content": find_others_system_prompt()},
+            {
+                "role": "user",
+                "content": find_others_user_prompt(target, cut(neighbors)),
+            },
         ],
         temperature=0.6,
         max_tokens=512,
     )
     arr = res.choices[0].message.content.split("[/INST]")
-    if (len(arr) < 1):
+    if len(arr) < 1:
         raise ValueError("No output")
-    
+
     return arr[1]
 
 
 # llm = init_llm()
-# target = """{uid: fb67383f-12de-4c07-a423-ed7faad8b7a3, summary:  The user has been working on projects related to 
-#             artificial intelligence (AI) and data analysis, primarily using tools and platforms provided by Nomic.ai. 
-#             They have explored Nomic's AI tools, Atlas and GPT4All, which focus on data exploration, model quality enhancement, 
+# target = """{uid: fb67383f-12de-4c07-a423-ed7faad8b7a3, summary:  The user has been working on projects related to
+#             artificial intelligence (AI) and data analysis, primarily using tools and platforms provided by Nomic.ai.
+#             They have explored Nomic's AI tools, Atlas and GPT4All, which focus on data exploration, model quality enhancement,
 #             and accessible AI. The user has also utilized Nomic Atlas for structuring and gaining insights from unstructured data,
-#               and has recently learned about the release of Nomic Embed, an open-source text embedding model. Additionally, the user 
-#               has been looking into the deepscatter project by nomic-ai, which is a library for creating zoomable, animated scatterplots 
-#               in the browser that can handle over a billion data points. They have accessed the GitHub repositories for both the deepscatter 
+#               and has recently learned about the release of Nomic Embed, an open-source text embedding model. Additionally, the user
+#               has been looking into the deepscatter project by nomic-ai, which is a library for creating zoomable, animated scatterplots
+#               in the browser that can handle over a billion data points. They have accessed the GitHub repositories for both the deepscatter
 #               project and Nomic AI's organization page, suggesting an interest in contributing to these projects or understanding their functionality in greater depth.}"""
 
 
