4d1fbeb8a02cbe72c3857e58cd8f98b270020f7a
zhouhelena <helenazhou04@gmail.com>
2024-02-16 21:47:12 -0500

    style popup

--- extension/popup/index.tsx
+++ extension/popup/index.tsx
@@ -11,32 +11,39 @@ function IndexPopup() {
 
   const goToDashboard = () => {
     console.log("hi")
-    window.open("chrome-extension://ggmokddcopnogabddgfgfpmdmjeappea/options.html")
+    window.open(
+      "chrome-extension://abcocpomaiddiceplohdmbfkpemkcjdj/options.html" // Change this to your own.
+    )
   }
 
   return (
-    <div className="p-2 bg-zinc-900 flex flex-col space-y-2 text-zinc-200" style={{
-      minWidth: 200,
-    }}>
-      <h2 className="text-xl font-bold text-center w-full">
-        Sit-Downs 💺
-      </h2>
-      <div className="flex flex-row w-full place-content-evenly space-x-2">
-        <div className="flex flex-col place-content-center align-middle">
-          <h4 className="text-lg">Toggle</h4>
-        </div>
-        <div className="flex flex-row">
-          <button className={` text-lg p-1 px-4 border
-            ${isOn ? "bg-zinc-200 text-zinc-900" : ""}
-          `} onClick={() => setStatus(true)}>On</button>
-          <button className={`text-lg p-1 px-4 border
-            ${!isOn ? "bg-zinc-200 text-zinc-900" : ""}
-          `} onClick={() => setStatus(false)}>Off</button>
+    <div
+      className="p-2 bg-zinc-900 flex flex-col space-y-2 text-zinc-200"
+      style={{
+        minWidth: 200
+      }}>
+      <h2 className="text-xl font-bold text-center w-full">Sit-Downs 💺</h2>
+      <div className="flex items-center justify-center">
+        <div className="flex items-center space-x-2 rounded-full px-2">
+          <button
+            className={`btn
+            ${isOn ? "bg-blue-500" : "bg-zinc-400"}`}
+            onClick={() => setIsOn(true)}>
+            On
+          </button>
+          <button
+            className={`btn
+            ${!isOn ? "bg-blue-500" : "bg-zinc-400"}`}
+            onClick={() => setIsOn(false)}>
+            Off
+          </button>
         </div>
       </div>
-      <button className="text-lg p-1 px-4 border bg-zinc-200 text-zinc-900" onClick={() => goToDashboard()}>
-        Dashboard
-      </button>
+      <div className="px-2 py-2">
+        <button className="btn" onClick={() => goToDashboard()}>
+          Dashboard
+        </button>
+      </div>
     </div>
   )
 }
--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
@@ -5,11 +7,45 @@ module.exports = {
   content: ["./**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        primary: "#ffd67d",
+        "primary-alt": "#b39044",
+        secondary: "#181c80",
+        "secondary-alt": "#0f0e43",
+        "background-light": "#06092b",
+        background: "#04000d",
+        "text-primary": "#ffffff",
+        "text-secondary": "#b7b1c9",
+        blur: "#b99ee7",
+        transparent: "transparent",
+        "border-color": "#ffffff10"
+      },
       fontFamily: {
-        inter: ['Inter', 'sans-serif']
+        inter: ["Inter", "sans-serif"]
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".btn": {
+          width: "100%",
+          minHeight: "3rem",
+          borderRadius: "0.75rem",
+          color: "#ffffff",
+          fontWeight: "500",
+          fontSize: "0.875rem",
+          background:
+            "radial-gradient(#ffffff12, #ffffff06), radial-gradient(ellipse at bottom, #b99ee740, transparent)",
+          backdropFilter: "blur(0.25rem)",
+          borderWidth: "1px",
+          borderColor: "#ffffff10",
+          transitionDuration: "300ms",
+          "&:hover": {
+            boxShadow: "inset 0 1rem 2rem 0 #ffffff10"
+          }
+        }
+      })
+    })
+  ]
 }
-
