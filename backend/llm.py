e4f45285c8112038fa17e83affcdf5ca114759fe
benjmnxu <benxu@seas.upenn.edu>
2024-02-17 09:53:51 -0500

    other k people

--- backend/llm.py
+++ backend/llm.py
@@ -2,8 +2,15 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from transformers import AutoTokenizer
+from prompts import find_others_system_prompt, find_others_user_prompt
 
 load_dotenv()
+
+def cut(self, webSequence):
+    tokenized = self.tokenizer(webSequence, return_tensors="pt")
+    if (len(tokenized.input_ids[0]) < 4000):
+        return webSequence
+    return self.tokenizer.decode(tokenized.input_ids[0][:4000])
 
 class llm:
     def __init__(self):
@@ -12,17 +19,11 @@ class llm:
             api_key=os.environ["BASE_TEN_API_KEY"],
             base_url=f"https://bridge.baseten.co/{model_id}/v1"
         )
-
-    def cut(self, webSequence):
-        tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-        tokenized = tokenizer(webSequence, return_tensors="pt")
-        if (len(tokenized.input_ids[0]) < 4096):
-            return webSequence
-        return tokenizer.decode(tokenized.input_ids[0][:4096])
+        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
 
     def summarize_webpage(self, title, url, body):
         webSequence = f"TITLE:{title},URL:{url},BODY:{body}"
-        webSequence = self.cut(webSequence[:4096])
+        webSequence = cut(webSequence)
         res = self.client.chat.completions.create(
         model="mistral-7b",
         messages=[
@@ -38,7 +39,7 @@ class llm:
         return arr[1]
     
     def summarize_day(self, summaries):
-        allSummaries = "`".join(summaries)[:4096]
+        allSummaries = "`".join(summaries)[:4000]
         res = self.client.chat.completions.create(
         model="mistral-7b",
         messages=[
@@ -52,4 +53,31 @@ class llm:
         if (len(arr) < 1):
             raise ValueError("No output")
         return arr[1]
+    
+    def other_k_people(self, master, slaves, k):
+        res = self.client.chat.completions.create(
+        model="mistral-7b",
+        messages=[
+            {"role": "system", "content": find_others_system_prompt(k)},
+            {"role": "user", "content": find_others_user_prompt(master, cut(self, slaves), k)}
+            ],
+            temperature=0.6,
+            max_tokens=512,
+        )
+        arr = res.choices[0].message.content.split("[/INST]")
+        if (len(arr) < 1):
+            raise ValueError("No output")
+        return arr[1]
+    
 
+
+# master_json="""
+# The user has been working on a project related to web development, specifically using CSS to fill an entire screen with a div. They have also been searching for general information on various topics, including "hello" and "hacker news."
+# Additionally, the user has been engaging with a variety of YouTube content related to survival and camping in extreme winter conditions. They have been accessing this content through a login page for ChatGPT, a conversational AI model, and a YouTube page with a miniplayer. The user has been interacting with the videos, such as playing, pausing, and adjusting playback speed and volume. They have also been using features such as sharing, liking, and adding videos to playlists. The user has been accessing this content both on a webpage and through a dynamic, interactive miniplayer.
+# """
+
+
+# slave_json = """
+#     7: { summary: The user has been working on a project related to Stable Diffusion, a feature of Hugging Face's Diffusers library. They have been consulting the official documentation page for Stable Diffusion, which provides a quick tour, installation instructions, and guides for loading and using pipelines, models, schedulers, and configuring them. They have also been following a tutorial in a Google Colab notebook, which is designed for running machine learning experiments using the Hugging Face library for diffusion models 
+# Additionally, the user has been exploring resources from Stability AI, a company that specializes in AI models. They have visited the news release page about the public release of the Stable Diffusion model, as well as the page for Stability AI Image Models, which provides information about the company's image models with a focus on the Stable Diffusion model. The user has also looked at the Stability AI website, which includes a navigation menu with links to sections like Models, Membership, API, and Company. }
+#     """
